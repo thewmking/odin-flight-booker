48959888ad7db71413fc367e6c1d74d2c1bcadea
Matthew King <thewmking@gmail.com>
2017-06-29 16:46:25 -0400

    Fix heroku email attempt #2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'peaceful-dawn-91419.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
+    #ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
